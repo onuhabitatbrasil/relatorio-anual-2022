89c84eb720131707eb6cd80dfc958da3996335a7
Gabriel Vaz de Melo <gabrielvazdemelo@gmail.com>
2023-03-15 17:01:52 -0300

    add sp territorio and jf history

--- docs/js/config.js
+++ docs/js/config.js
@@ -56,13 +56,13 @@ var config = {
       //title: 'Abertura',
       bookmark: 'Abertura',
       description: '\
-      <div class="light">\
-        <div class="container-history text-left p-bottom">\
-          <div class="row no-gutters">\
-            <div class="col-lg-7 pl-lg-4">\
+      <div class="light-branco">\
+        <div class="container-history p-bottom">\
+          <div class="row no-gutters p-left">\
+            <div class="col-lg-7">\
               <div class="row">\
                 <div class="col-md-12">\
-                  <h1>Palavras de Abertura</h1>\
+                  <h1 class="azul-habitat">Palavras de Abertura</h1>\
                 </div>\
               </div>\
               <div class="row">\
@@ -71,8 +71,8 @@ var config = {
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam placerat justo sit amet lorem elementum, imperdiet condimentum nunc tincidunt. <strong>Mauris pulvinar purus leo, eu auctor felis consectetur vel. In vehicula vulputate felis vel aliquet.</strong> Maecenas interdum molestie ligula a maximus. In nulla tellus, sagittis sed magna in, cursus pretium diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>\
                 </div>\
                 <div class="col-md-6">\
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam placerat justo sit amet lorem elementum, imperdiet condimentum nunc tincidunt. Mauris pulvinar purus leo, eu auctor felis consectetur vel. In vehicula vulputate felis vel aliquet. Maecenas interdum molestie ligula a maximus. <strong>In nulla tellus, sagittis sed magna in, cursus pretium diam.</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam placerat justo sit amet lorem elementum, imperdiet condimentum nunc tincidunt. Mauris pulvinar purus leo, eu auctor felis consectetur vel. In vehicula vulputate felis vel aliquet. Maecenas interdum molestie ligula a maximus. In nulla tellus, sagittis sed magna in, cursus pretium diam.</p>\
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam placerat justo sit amet lorem elementum, imperdiet condimentum nunc tincidunt. Mauris pulvinar purus leo, eu auctor felis consectetur vel. In vehicula vulputate felis vel aliquet.</p>\
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam placerat justo sit amet lorem elementum, imperdiet condimentum nunc tincidunt. Mauris pulvinar purus leo, eu auctor felis consectetur vel. In vehicula vulputate felis vel aliquet. Maecenas interdum molestie ligula a maximus. <strong>In nulla tellus, sagittis sed magna in, cursus pretium diam.</strong> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam placerat justo sit amet lorem elementum, imperdiet condimentum nunc tincidunt. Mauris pulvinar purus leo, eu auctor felis consectetur vel. In vehicula vulputate felis vel aliquet. Maecenas interdum molestie ligula a maximus. In nulla tellus, sagittis sed magna in, cursus pretium diam.</p>\
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam placerat justo sit amet lorem elementum, imperdiet condimentum nunc tincidunt. Mauris pulvinar purus leo, eu auctor felis consectetur vel. In vehicula vulputate felis vel aliquet.</p>\
                 </div>\
               </div>\
           </div>\
@@ -113,11 +113,11 @@ var config = {
         bookmark: 'ONU-Habitat Brasil',
         image: false,
         description: '\
-        <div class="light rounded-lg shadow-lg">\
+        <div class="light-branco rounded-lg shadow-lg">\
           <div class="container p-right-left p-bottom">\
-            <h1 class=>ONU-Habitat Brasil</h1>\
+            <h1 class="azul-habitat">ONU-Habitat Brasil</h1>\
             <p>O ONU-Habitat está presente no Brasil há mais de 20 anos, atuando em projetos relacionados a diversos temas urbanos em cidades de todo o país. O Escritório Regional para América Latina e o Caribe e o escritório para o Brasil e Cone Sul estão localizados no Rio de Janeiro.</p>\
-            <img class="p-top p-bottom" src="images/logo_onuhabitat_branco.png" alt="Logo ONU-Habitat em Português"/>\
+            <img class="p-top p-bottom" src="images/logo_onuhabitat.png" alt="Logo ONU-Habitat em Português"/>\
           </div>\
         </div>\
         ',
@@ -233,7 +233,7 @@ var config = {
         description: '\
         <div class="light rounded-lg shadow-lg">\
           <div class="container p-right-left p-bottom">\
-            <h1>Juiz de Fora</h1>\
+            <h1 class="laranja">Juiz de Fora</h1>\
             <p>Maior cidade da Zona da Mata de Minas Gerais, Juiz de Fora é reconhecida pelo seu povo acolhedor, pelo espírito vanguardista e pelas diversas experiências que proporciona. Com uma população de aproximadamente 577 mil habitantes e uma localização estratégica, ela se apresenta como capital regional, exercendo influência nos municípios do entorno.</p>\
             <p>Manchester Mineira e Princesa de Minas são alguns títulos que demonstram sua identidade pioneira e singular. O município foi um importante polo industrial, abrigando diversas fábricas ao longo de sua expansão, como Marmelos Zero, a primeira grande usina hidrelétrica da América do Sul.</p>\
             <p>Andar pelas ruas é reconhecer em cada canto um pouco da sua história de 172 anos, da sua arquitetura tradicional, dos seus contornos culturais. Mas é também perceber as inúmeras desigualdades territoriais. Conhecer o seu espaço, suas contradições e seus moradores possibilita que Juiz de Fora seja transformada para garantir o direito à cidade a sua população. Para isso, o projeto Territórios da Cidadania atua em 141 microterritórios historicamente excluídos do planejamento urbano. Neste contexto, os dados se transformam em políticas públicas capazes de melhorar a vida de todos os juizforanos.</p>\
@@ -245,7 +245,7 @@ var config = {
           zoom: 11,
           pitch: 45.00,
           bearing: 0.00,
-          speed: 0.75
+          speed: 2
       },
       mapAnimation: 'flyTo',
       rotateAnimation: false,
@@ -264,22 +264,136 @@ var config = {
               opacity: 0
           }
       ]
-  },
+    },
+
+    {
+      id: 'historia-jf',
+      alignment: 'full',
+      hidden: false,
+      //title: 'Abertura',
+      bookmark: 'Juiz de Fora',
+      description: '\
+      <div class="light">\
+        <div class="container-history text-left">\
+          <div class="row no-gutters p-left">\
+            <div class="col-lg-8">\
+              <div class="row">\
+                <div class="col-md-12">\
+                  <h1 class="laranja">De volta às origens</h1>\
+                </div>\
+              </div>\
+              <div class="row p-bottom">\
+                <div class="col-md-6">\
+                    <p>Em 2022, a geógrafa Elaine Cristóvão Coelho viajou quase 1,5 mil km de Salvador para voltar ao lugar onde sua trajetória começou. Ela cresceu no bairro Marumbi, na zona leste de Juiz de Fora (MG), uma região que historicamente apresenta alta vulnerabilidade social. Atuando como supervisora de campo no ONU-Habitat, ela percorreu uma extensão equivalente a 350 campos de futebol em vias íngremes, sinuosas, com características que mesclam rural e urbano. Tudo para promover o desenvolvimento socioeconômico e garantir o acesso a programas de cidadania em 141 microterritórios da cidade.</p>\
+                    <p>O Territórios da Cidadania, projeto no qual atuou, é uma parceria com a Prefeitura de Juiz de Fora para realizar um diagnóstico dos espaços urbanos, das moradias e da população que reside nesses locais. Atuando como uma ferramenta de planejamento e gestão, os dados coletados auxiliam na superação de desigualdades socioespaciais, embasando a formulação de políticas públicas e priorizações de investimento.</p>\
+                    <p>A conexão de Elaine com trabalhos de impacto social parte da sua própria vivência. Enquanto crescia, sua casa era de chão batido, as ruas não tinham asfalto e, para estudar, seu padrinho ajudava na compra dos cadernos. A educação era um instrumento de transformação social. “Venho de uma linhagem de empregadas domésticas: minha mãe, minha avó, minhas tias e eu também fomos empregadas domésticas. Sempre tive fé na educação porque sabia que ela poderia me salvar profissional e economicamente”.</p>\
+                    <p>Elaine se formou geógrafa e, vinte anos depois, retornou ao território para atuar como profissional do projeto. A contratação de habitantes dos microterritórios no projeto possibilitou a entrada de pessoas especializadas que entendem melhor a realidade dessas localidades e se identificam</p>\
+                </div>\
+                <div class="col-md-6">\
+                    <p>com sua população. Essa é também uma forma de dar um retorno para regiões historicamente invisibilizadas no planejamento urbano da cidade.</p>\
+                    <p>Elaine entende que as informações coletadas não são só números. Por trás, há uma pessoa com frustrações, problemas e sonhos. “É preciso analisar o que aquele dado quer dizer. Existe um contexto que ajuda a explicar esse indicador. As pessoas precisam e querem ser ouvidas. Por isso sempre gostei muito de acompanhar o trabalho de campo”.</p>\
+                    <p>As metodologias aplicadas na cidade — Mapa Rápido Participativo e Perfil Socioeconômico — são participativas. Para Elaine, equilibrar a visão técnica com a percepção da população é o primeiro passo para garantir o sucesso do projeto: perceber que a Prefeitura busca conhecer o indivíduo e suas necessidades é uma forma de trazer como retorno políticas públicas mais assertivas. “Nesse diagnóstico, é importante se esvaziar das nossas próprias percepções. Eu vou ouvir do outro o que ele pensa sobre si e sobre onde ele mora”, explica. “Não me lembro disso já ter sido feito aqui; é uma inovação da qual fico muito feliz por ter participado. Sei que vai mudar a vida de muita gente”.</p>\
+                    <p>E mudou a vida dela também. “Parecia uma possibilidade tão remota trabalhar na ONU, era algo que eu nem sonhava. Isso me dá esperança de que pessoas como eu, mulheres negras, de origem pobre, podem alcançar esses espaços”, comemora. “Eu parti de muito longe, é como uma chancela da minha trajetória: tá vendo onde você pode chegar? Isso só me motiva a buscar mais e entender que eu posso, que as minhas também podem. Acho que eu sou parecida com quem sempre sonhei ser”.</p>\
+                </div>\
+              </div>\
+          </div>\
+            <div class="col-lg-4 align-self-end">\
+                <img src="images/historia-jf.png" alt="" class="img-fluid"/>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+      ',
+      location: {
+        center: [-43.34615, -21.75693],
+        zoom: 11,
+        pitch: 45.00,
+        bearing: 0.00,
+        speed: 0.75
+          // flyTo additional controls-
+          // These options control the flight curve, making it move
+          // slowly and zoom out almost completely before starting
+          // to pan.
+          //speed: 2, // make the flying slow
+          //curve: 1, // change the speed at which it zooms out
+      },
+      mapAnimation: 'flyTo',
+      rotateAnimation: false,
+      spinGlobe: false,
+      mapInteractive: false,
+      callback: '',
+      onChapterEnter: [
+        {
+            layer: 'microterritorios',
+            opacity: 0.5
+        }
+      ],
+      onChapterExit: [
+        {
+            layer: 'microterritorios',
+            opacity: 0
+        }
+      ]
+    },
+
+    {
+      id: 'territorio-sp',
+        alignment: 'left',
+        hidden: false,
+        //title: 'Escritório ONU-Habitat Brasil',
+        bookmark: 'Territorios São Paulo',
+        image: false,
+        description: '\
+        <div class="light rounded-lg shadow-lg">\
+          <div class="container p-right-left p-bottom">\
+            <h1 class="laranja">São Paulo</h1>\
+            <p>Uma metrópole global profundamente dinâmica, digitalizada e complexa. São Paulo é lar de 12,4 milhões de pessoas, distribuídas em uma área de mais de 1,5 mil quilômetros quadrados. Somada aos outros 38 municípios que compõem sua Região Metropolitana, ela concentra metade da população do Estado e mais de 30% do PIB do país.</p>\
+            <p>Ao longo do século 19, durante o Ciclo do Café, o rápido crescimento populacional e produtivo da cidade consolidou sua mancha urbana, comprometendo os biomas locais – especialmente o de Mata Atlântica, predominante na região. Além do espaço geográfico, também se perderam inúmeras espécies de fauna e flora que faziam parte desse ecossistema.</p>\
+            <p>Essa trajetória, no entanto, está em transformação. Hoje, apesar da fama de selva de pedra ser parte incontornável do imaginário da cidade, quase metade do território de São Paulo corresponde à cobertura verde, e quase um terço é território rural. Em 30 anos, o número de parques cresceu de 32 para 111, o que ilustra a preocupação do município em incentivar o desenvolvimento e a ocupação dos espaços verdes pelas pessoas.</p>\
+            <p>Neste processo, São Paulo tem mantido em vista a necessidade de avaliar a qualidade de parques e áreas de conservação, promovendo espaços verdes de maneira mais igualitária para seus milhões de moradores. Além disso, outras ações também estão em foco: capacitar instituições e sociedade civil para criar, gerir e monitorar a qualidade dos equipamentos urbanos; identificar áreas em potencial para desenvolver novos espaços públicos verdes e garantir a participação da população de forma equilibrada com relação a raça, classe, gênero e território.</p>\
+          </div>\
+        </div>\
+        ',
+        location: {
+          center: [-46.68480, -23.64944],
+          zoom: 10.16,
+          pitch: 45.00,
+          bearing: 0.00,
+          speed: 2
+        },
+        mapAnimation: 'flyTo',
+        rotateAnimation: false,
+        spinGlobe: false,
+        mapInteractive: true,
+        callback: '',
+        onChapterEnter: [
+            {
+                layer: 'sp-parques',
+                opacity: 1
+            }
+        ],
+        onChapterExit: [
+            {
+                layer: 'sp-parques',
+                opacity: 0
+            }
+        ]
+    },
 
     {
       id: 'historia-sp',
       alignment: 'full',
       hidden: false,
       //title: 'Abertura',
-      bookmark: 'Parcerias São Paulo',
+      bookmark: 'História São Paulo',
       description: '\
       <div class="light">\
-        <div class="container-history text-left">\
-          <div class="row no-gutters">\
-            <div class="col-lg-7">\
+        <div class="container-history">\
+          <div class="row no-gutters p-left">\
+            <div class="col-lg-8">\
               <div class="row">\
                 <div class="col-md-12">\
-                  <h1>Horizonte mais verde na metrópole global</h1>\
+                  <h1 class="laranja">Horizonte mais verde na metrópole global</h1>\
                 </div>\
               </div>\
               <div class="row p-bottom">\
@@ -297,7 +411,7 @@ var config = {
                 </div>\
               </div>\
           </div>\
-            <div class="col-lg-5 align-self-end">\
+            <div class="col-lg-4 align-self-end">\
                 <img src="images/sao_paulo.png" alt="" class="img-fluid"/>\
             </div>\
           </div>\
@@ -305,25 +419,29 @@ var config = {
       </div>\
       ',
       location: {
-          center: [-22.32528, -2.14945],
-          zoom: 1.5,
-          pitch: 0,
-          bearing: 0,
-          speed: 2
-          // flyTo additional controls-
-          // These options control the flight curve, making it move
-          // slowly and zoom out almost completely before starting
-          // to pan.
-          //speed: 2, // make the flying slow
-          //curve: 1, // change the speed at which it zooms out
+        center: [-46.68480, -23.64944],
+        zoom: 10.16,
+        pitch: 45.00,
+        bearing: 0.00,
+        speed: 2
       },
       mapAnimation: 'flyTo',
       rotateAnimation: false,
       spinGlobe: false,
       mapInteractive: false,
       callback: '',
-      onChapterEnter: [],
-      onChapterExit: []
+      onChapterEnter: [
+          {
+              layer: 'sp-parques',
+              opacity: 0.5
+          }
+      ],
+      onChapterExit: [
+          {
+              layer: 'sp-parques',
+              opacity: 0
+          }
+      ]
     },
 
     {
@@ -335,11 +453,11 @@ var config = {
       description: '\
       <div class="light">\
         <div class="container-history text-left">\
-          <div class="row no-gutters">\
-            <div class="col-lg-7">\
+          <div class="row no-gutters p-left">\
+            <div class="col-lg-8">\
               <div class="row">\
                 <div class="col-md-12">\
-                  <h1>Infinitas conexões entre o espaço público e o sideral</h1>\
+                  <h1 class="laranja">Infinitas conexões entre o espaço público e o sideral</h1>\
                 </div>\
               </div>\
               <div class="row p-bottom">\
@@ -359,7 +477,7 @@ var config = {
                 </div>\
               </div>\
           </div>\
-            <div class="col-lg-5 align-self-end">\
+            <div class="col-lg-4 align-self-end">\
                 <img src="images/pernambuco.png" alt="" class="img-fluid"/>\
             </div>\
           </div>\
